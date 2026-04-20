0afbcd332c92f6743aaa3057ec65d8626be89253
Oscar David Morales <morales.oscardavid@gmail.com>
2026-04-19 23:02:25 -0300

    fix

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -100,6 +100,11 @@
                         <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path></svg>
                         <span class="font-bold text-sm">{{ __('Logística') }}</span>
                     </x-responsive-nav-link>
+
+                    <x-responsive-nav-link :href="route('events.follow-up')" :active="request()->routeIs('events.follow-up')" class="flex items-center p-4 rounded-2xl {{ request()->routeIs('events.follow-up') ? 'bg-indigo-50 text-indigo-600 border-none' : 'text-gray-600 dark:text-gray-300 border-none hover:bg-gray-50 dark:hover:bg-gray-800' }}">
+                        <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path></svg>
+                        <span class="font-bold text-sm">{{ __('Seguimiento') }}</span>
+                    </x-responsive-nav-link>
                 </div>
             </div>
 
