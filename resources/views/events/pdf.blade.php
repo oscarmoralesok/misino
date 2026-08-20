7634a84749a3aa2b883584dd4201e1b171998a51
Oscar Morales <morales.oscardavid@gmail.com>
2026-08-20 19:38:50 +0200

    fix

--- resources/views/events/pdf.blade.php
+++ resources/views/events/pdf.blade.php
@@ -275,8 +275,8 @@
         @endif
 
         <div class="alert-box">
-            <strong>IMPORTANTE:</strong>
-            {{-- Las guirnaldas de globos y elementos decorativos forman parte exclusiva del servicio de alquiler.  --}}
+            {{-- <strong>IMPORTANTE:</strong> --}}
+            Las guirnaldas de globos y elementos decorativos forman parte exclusiva del servicio de alquiler. 
             {{-- No está permitido el retiro, traslado o manipulación de las mismas fuera del lugar del evento por parte del cliente o invitados. --}}
         </div>
 
